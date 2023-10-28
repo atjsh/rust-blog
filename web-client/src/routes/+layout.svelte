62f57e2c9fc144ee35c76320cc0b59570055fe74
전성훈 / SUNG HOON CHON <letaem77@gmail.com>
2023-10-28 21:39:13 +0900

    feat: add loading indicator

--- web-client/src/routes/+layout.svelte
+++ web-client/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import type { LayoutServerData } from './$types';
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { navigating } from '$app/stores';
 
 	inject({ mode: dev ? 'development' : 'production' });
 
@@ -10,6 +11,12 @@
 
 <main>
 	<header>
+		{#if $navigating}
+			<div class="loading">
+				<div class="spin">⏳</div>
+				<div>loading...</div>
+			</div>
+		{/if}
 		<a href="/">This is rust-blog</a><br />
 		{#if data.authed}
 			<a href="/profile">Profile</a> /
@@ -22,3 +29,31 @@
 	<slot />
 	<footer>2023 ATJSH. All rights reserved</footer>
 </main>
+
+<style>
+	.loading {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		background: rgba(0, 0, 0, 0.8);
+		color: white;
+		padding: 1em;
+		display: flex;
+		gap: 0.5em;
+	}
+
+	.spin {
+		animation: spin 1s linear infinite;
+	}
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
