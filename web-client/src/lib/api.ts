19e28880e154c1ce121c8489e3e95e682c006649
ATJSH <letaem77@gmail.com>
2023-11-06 11:20:40 +0900

    feat: update api.ts

--- web-client/src/lib/api.ts
+++ web-client/src/lib/api.ts
@@ -1,19 +1,8 @@
 import { PUBLIC_SERVER_URL } from '$env/static/public';
-import returnFetch from 'return-fetch';
+import axios from 'axios';
 
-const serverFetch = returnFetch({
-	baseUrl: PUBLIC_SERVER_URL,
-	interceptors: {
-		request: async (requestArgs) => {
-			return [
-				requestArgs[0],
-				{
-					...requestArgs[1],
-					credentials: 'include'
-				}
-			];
-		}
-	}
+const serverAxios = axios.create({
+	baseURL: PUBLIC_SERVER_URL
 });
 
 export type GetCategoryResponseData = {
@@ -82,68 +71,51 @@ export type GetAuthedPayload = {
 };
 
 export async function getAccessToken(payload: GetAuthedPayload): Promise<string> {
-	const response = await serverFetch(`/auth/access-token`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(payload)
-	});
+	const response = await serverAxios.post(`/auth/access-token`, payload);
 
 	if (response.status !== 200) {
 		throw Error('Failed to get access token');
 	}
 
-	return response.text();
+	return response.data;
 }
 
 export async function getCurrentAuthedWriterId(accessToken: string): Promise<number> {
-	const response = await serverFetch('/profile', {
-		method: 'GET',
+	const response = await serverAxios.get('/profile', {
 		headers: {
 			Authorization: `Bearer ${accessToken}`
 		}
 	});
 
-	return Number(await response.text());
+	return Number(await response.data);
 }
 
 export async function logout() {
-	await serverFetch(`/auth`, {
-		method: 'DELETE'
-	});
+	await serverAxios.delete(`/auth`);
 }
 
 export async function getCategories(): Promise<GetCategoryResponseData[]> {
 	return (await (
-		await serverFetch(`/category`, {
-			method: 'GET'
-		})
-	).json()) as GetCategoryResponseData[];
+		await serverAxios.get(`/category`)
+	).data) as GetCategoryResponseData[];
 }
 
 export async function getCategory(categoryId: number): Promise<GetCategoryResponseData> {
 	return (await (
-		await serverFetch(`/category/${categoryId}`, {
-			method: 'GET'
-		})
-	).json()) as GetCategoryResponseData;
+		await serverAxios.get(`/category/${categoryId}`)
+	).data) as GetCategoryResponseData;
 }
 
 export async function getCategoryPosts(categoryId: number): Promise<GetCategoryPostsResponseData> {
 	return (await (
-		await serverFetch(`/category/${categoryId}/posts`, {
-			method: 'GET'
-		})
-	).json()) as GetCategoryPostsResponseData;
+		await serverAxios.get(`/category/${categoryId}/posts`)
+	).data) as GetCategoryPostsResponseData;
 }
 
 export async function getPost(postId: number): Promise<GetPostResponseData> {
 	return (await (
-		await serverFetch(`/post/${postId}`, {
-			method: 'GET'
-		})
-	).json()) as GetPostResponseData;
+		await serverAxios.get(`/post/${postId}`)
+	).data) as GetPostResponseData;
 }
 
 export async function createPost(
@@ -153,19 +125,20 @@ export async function createPost(
 	accessToken: string
 ): Promise<GetPostResponseData> {
 	return (await (
-		await serverFetch(`/post`, {
-			method: 'POST',
-			body: JSON.stringify({
+		await serverAxios.post(
+			`/post`,
+			{
 				title,
 				content,
 				category_id: categoryId
-			}),
-			headers: {
-				Authorization: `Bearer ${accessToken}`,
-				'Content-Type': 'application/json'
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${accessToken}`
+				}
 			}
-		})
-	).json()) as GetPostResponseData;
+		)
+	).data) as GetPostResponseData;
 }
 
 export async function updatePost(
@@ -175,25 +148,25 @@ export async function updatePost(
 	content: string,
 	accessToken: string
 ): Promise<GetPostResponseData> {
-	const response = await serverFetch(`/post/${postId}`, {
-		method: 'PATCH',
-		body: JSON.stringify({
+	const response = await serverAxios.patch(
+		`/post/${postId}`,
+		{
 			title,
 			content,
 			category_id: categoryId
-		}),
-		headers: {
-			Authorization: `Bearer ${accessToken}`,
-			'Content-Type': 'application/json'
+		},
+		{
+			headers: {
+				Authorization: `Bearer ${accessToken}`
+			}
 		}
-	});
+	);
 
-	return (await response.json()) as GetPostResponseData;
+	return response.data as GetPostResponseData;
 }
 
 export async function deletePost(postId: number, accessToken: string) {
-	await serverFetch(`/post/${postId}`, {
-		method: 'DELETE',
+	await serverAxios.delete(`/post/${postId}`, {
 		headers: {
 			Authorization: `Bearer ${accessToken}`
 		}
@@ -201,27 +174,16 @@ export async function deletePost(postId: number, accessToken: string) {
 }
 
 export async function getWriter(writerId: number): Promise<GetWriterResponseData> {
-	return (await (
-		await serverFetch(`/writer/${writerId}`, {
-			method: 'GET'
-		})
-	).json()) as GetWriterResponseData;
+	return (await serverAxios.get(`/writer/${writerId}`)).data as GetWriterResponseData;
 }
 
 export async function getWriterPosts(writerId: number): Promise<GetWriterPostResponseData> {
-	return (await (
-		await serverFetch(`/writer/${writerId}/posts`, {
-			method: 'GET'
-		})
-	).json()) as GetWriterPostResponseData;
+	return (await serverAxios.get(`/writer/${writerId}/posts`)).data as GetWriterPostResponseData;
 }
 
 export async function updateProfile(email: string | null, description: string | null) {
-	await serverFetch(`/writer/profile`, {
-		method: 'PUT',
-		body: JSON.stringify({
-			email,
-			description
-		})
+	await serverAxios.put(`/writer/profile`, {
+		email,
+		description
 	});
 }
