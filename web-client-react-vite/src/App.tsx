764226c81a19b7fea6bfaf31453c870aa1f7c76f
ATJSH <letaem77@gmail.com>
2023-12-07 13:54:39 +0900

    feat: progress update

--- web-client-react-vite/src/App.tsx
+++ web-client-react-vite/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <header>
+        <a href="/" className="title-a">
+          <div className="title-content">
+            <div className="icon">🫠</div>
+            <code>blog.atj.sh</code>
+          </div>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </header>
+      <main>
+        <h1>전성훈의 블로그</h1>
+        <ul>
+          <li>
+            <h2>
+              <a href={`/category`}>ㅇㅇ</a> 카테고리의 글 목록
+            </h2>
+          </li>
+        </ul>
+      </main>
+      <footer></footer>
     </>
-  )
+  );
 }
-
-export default App
