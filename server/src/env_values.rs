9b5318fd2f3092d0e9977a9cd8378fd66cdcdebe
ATJSH <letaem77@gmail.com>
2023-12-15 11:58:09 +0900

    feat: revert rust oauth logic

--- server/src/env_values.rs
+++ server/src/env_values.rs
@@ -1,6 +1,3 @@
 pub const DATABASE_URL: &str = "DATABASE_URL"; // Database URL to connecting to Postgres database
 pub const JWT_SECRET: &str = "JWT_SECRET"; // Secret key for jwt signing
 pub const WEB_CLIENT_URL: &str = "WEB_CLIENT_URL"; // URL of the web client; used for CORS policy
-pub const GOOGLE_CLIENT_ID: &str = "GOOGLE_CLIENT_ID"; // Google OAuth client ID
-pub const GOOGLE_CLIENT_SECRET: &str = "GOOGLE_CLIENT_SECRET"; // Google OAuth client secret
-pub const GOOGLE_REDIRECT_URI: &str = "GOOGLE_REDIRECT_URI"; // Google OAuth redirect URI
