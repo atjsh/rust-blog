807b0ceea281f9794337b81ee0017bda0a17c894
전성훈 / SUNG HOON CHON <letaem77@gmail.com>
2024-03-02 05:11:34 +0900

    feat: update styling

--- web-client-sveltekit/src/routes/+page.svelte
+++ web-client-sveltekit/src/routes/+page.svelte
@@ -1,8 +1,12 @@
-<p>읽고 싶은 페이지를 선택하세요.</p>
+<div />
 
 <style>
-	p {
-		font-size: 1.2em;
-		padding: 1em;
+	div {
+		background-color: #b6b6b6;
+		background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300"><text x="150" y="210" text-anchor="middle" transform="rotate(45 150 150)" font-size="180" font-family="sans-serif" fill="black" fill-opacity="0.03">PB</text></svg>');
+		background-repeat: repeat;
+		background-size: 4em;
+		height: 100vh;
+		width: 100%;
 	}
 </style>
