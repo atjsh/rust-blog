cb53a72f7a5b715e1d16fb2974be116e87791d98
전성훈 / SUNG HOON CHON <letaem77@gmail.com>
2024-05-04 14:42:39 +0900

    feat: update nav style

--- web-client-sveltekit/src/routes/+layout.svelte
+++ web-client-sveltekit/src/routes/+layout.svelte
@@ -107,8 +107,8 @@
 		background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300"><text x="150" y="210" text-anchor="middle" transform="rotate(45 150 150)" font-size="180" font-family="sans-serif" fill="black" fill-opacity="0.03">PB</text></svg>');
 		background-repeat: repeat;
 		width: 30rem;
-		min-height: 100vh;
-		max-height: 100vh;
+		height: 100vh;
+		height: 100dvh;
 		box-sizing: border-box;
 		overflow-y: auto;
 
