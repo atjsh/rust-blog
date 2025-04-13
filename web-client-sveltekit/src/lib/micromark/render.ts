49ba3e88059fbb8e2c81468a6f0f71e3bffd7a5e
전성훈 / SUNG HOON CHON <letaem77@gmail.com>
2025-04-13 21:51:07 +0900

    feat: update renderer

--- web-client-sveltekit/src/lib/micromark/render.ts
+++ web-client-sveltekit/src/lib/micromark/render.ts
@@ -1,10 +1,12 @@
 import { micromark } from 'micromark';
 import { directive, directiveHtml } from 'micromark-extension-directive';
+import { gfm, gfmHtml } from 'micromark-extension-gfm';
 import { imgTagHandler } from './tag-handlers';
 
 export function getHTMLFromMarkdown(markdown: string): string {
 	return micromark(markdown, {
-		extensions: [directive()],
-		htmlExtensions: [directiveHtml({ img: imgTagHandler })]
+		allowDangerousHtml: true,
+		extensions: [directive(), gfm()],
+		htmlExtensions: [directiveHtml({ img: imgTagHandler }), gfmHtml()]
 	});
 }
