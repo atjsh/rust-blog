b45fdbb146a0d4f8cd03703a367577ae42f258a0
전성훈 / SUNG HOON CHON <letaem77@gmail.com>
2025-05-17 23:30:51 +0900

    feat: update layout (category sort)
    Signed-off-by: 전성훈 / SUNG HOON CHON <letaem77@gmail.com>

--- web-client-sveltekit/src/routes/+layout.server.ts
+++ web-client-sveltekit/src/routes/+layout.server.ts
@@ -5,10 +5,20 @@ export const load: LayoutServerLoad = async ({ cookies }) => {
 	const authed = cookies.get('accessToken') !== undefined;
 	const categories = await getCategories();
 
+	const sortedCategories = categories.sort((a, b) => {
+		if (new Date(a.created_at) < new Date(b.created_at)) {
+			return 1;
+		} else if (new Date(a.created_at) > new Date(b.created_at)) {
+			return -1;
+		} else {
+			return 0;
+		}
+	});
+
 	return {
 		authed: authed,
 		categories: await Promise.all(
-			categories.map(async (category) => ({
+			sortedCategories.map(async (category) => ({
 				...category,
 				posts: await getCategoryPosts(category.id)
 			}))
